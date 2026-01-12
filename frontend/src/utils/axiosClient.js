6112c908494c85af7f216ec7228ce86e9472fe54
Asif <asif.mhaldar03@gmail.com>
2026-01-12 13:06:16 +0530

    done

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosClient =  axios.create({
-    baseURL: ['http://localhost:3000', "https://leetcode-clone-git-main-asif-mhaldars-projects.vercel.app/"],
+    baseURL: ['http://localhost:3000', process.env.REACT_APP_URL],
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
